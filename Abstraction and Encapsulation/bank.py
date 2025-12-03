ba04bad94336ed19706d7c210379548c55b592f9
Basu <basavarajumachagi99@gmail.com>
2025-12-03 19:32:31 +0530

    Updating Bank Balance(Setters and Getters)

--- Abstraction and Encapsulation/bank.py
+++ Abstraction and Encapsulation/bank.py
@@ -1,19 +1,26 @@
 class Bank:
     def __init__(self,balance):
         self.__balance=balance
+    def balance(self):
+        print(self.__balance) #By Using Getters we Retrieving the balance
+    def update(self,balance):
+        self.__balance=balance #Using setters we updating a balance
     def deposit(self,amt):
-        if amt>0:
+        if amt >0:
             self.__balance+=amt
-            print(f"The balance {self.__balance}")
+            print(self.__balance)
         else:
-            print("The amt must be in positive")
+            print("Amt should be in Positive") 
     def withdraw(self,amt):
         if amt>0:
             self.__balance-=amt
-            print(f"Balance is {self.__balance}")
+            print(self.__balance)
         else:
-            print("Amt should be in positive")
+            print("Positive nmbers only")   
+        
 a=Bank(15000)
+a.balance()#Here We called the function in which bank balance is display for users by (Getters)
 a.deposit(5000)
+a.update(12500)#Here we called the function in which bank balance updated By (Setters)
 a.withdraw(12000)
 
